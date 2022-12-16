e856d45f419ab491190141e9868027260417cc33
ayedibtissem <116677898+ayedibtissem@users.noreply.github.com>
2022-12-16 17:06:23 +0100

    Create Photo.js

---
+++ Photo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ts from "./pro.jpg"
+function Photo (){
+    return <div className="p"> 
+        <img src= {ts}   alt="pro"width={200}/>
+         </div>
+}
+export default Photo;
